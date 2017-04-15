d48c869df4e1f56ea3ff6252a7eee8686b0b6164
mfinkels <matiif99@icloud.com>
2017-04-15 17:29:00 -0300

    Android Studio

---
+++ Morfando/app/src/main/java/com/example/matias/morfando/ActividadPrincipal.java
@@ -0,0 +1,18 @@
+package com.example.matias.morfando;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActividadPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad_principal);
+    }
+
+    public void perfilBoton(View viewRecibida) {
+
+    }
+}
