f3431c65b1a94d81bf689bff0ed6f0b1c6531c67
alu654 <alanffederico@hotmail.com>
2017-04-28 17:02:18 -0300

    Paquete de datos conectado con el perfil

--- Morfando/app/src/main/java/com/example/matias/morfando/activityProfile.java
+++ Morfando/app/src/main/java/com/example/matias/morfando/activityProfile.java
@@ -2,6 +2,8 @@ package com.example.matias.morfando;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activityProfile extends AppCompatActivity {
 
@@ -10,5 +12,45 @@ public class activityProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle("Inicio");
         setContentView(R.layout.activity_profile);
+
+        Bundle datosRecividos;
+        datosRecividos=this.getIntent().getExtras();
+
+        String nombreRecivido=datosRecividos.getString("name");
+
+        /*lastname
+
+         */
+        String lastName=datosRecividos.getString("lastname");
+
+
+        /*
+        email
+         */
+        String mail =datosRecividos.getString("email");
+
+            /*
+             password
+             */
+
+        String password =datosRecividos.getString("password");
+        /*
+        confirm_password
+         */
+        String confirm_password =datosRecividos.getString("confirm_password");
+
+        /*
+        phone
+         */
+
+        String phone =datosRecividos.getString("phone");
+
+        TextView nombre;
+        nombre=(TextView)findViewById(R.id.user_profile_name);
+        nombre.getText().toString();
+
+        nombre==nombreRecivido.toString();
+
+
     }
 }
