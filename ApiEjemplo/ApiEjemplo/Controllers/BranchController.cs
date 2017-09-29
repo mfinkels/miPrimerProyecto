d83e3c856d87adb79f429b7d0a8e2adb9c6a3127
mfinkels <matiif99@icloud.com>
2017-09-29 09:38:29 -0300

    fix azure

--- ApiEjemplo/ApiEjemplo/Controllers/BranchController.cs
+++ ApiEjemplo/ApiEjemplo/Controllers/BranchController.cs
@@ -101,7 +101,7 @@ namespace ApiEjemplo.Controllers
             {
                 return NotFound();
             }
-            TypeMenuData.delete(id);
+            TypeMenuData.Delete(id);
             return Ok();
         }
 
