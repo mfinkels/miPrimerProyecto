5c9e99204ada31fef2229f70e8e03464503ab957
mfinkels <matiif99@icloud.com>
2017-11-30 18:06:24 -0300

    fixing step one

--- Morfando/app/src/main/java/com/morfando/android/morfando/Interface/asyncTaskCompleted.java
+++
@@ -1,9 +0,0 @@
-package com.morfando.android.morfando.Interface;
-
-/**
- * Created by Matias on 8/9/2017.
- */
-
-public interface asyncTaskCompleted<Object> {
-    void onPostAsyncTask(Object result);
-}
